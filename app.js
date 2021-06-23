ad97eb83fdcd6330b0ff5f275b2e11dddfafcb14
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-06-23 15:53:25 -0300

    intial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+//Loading modules
+const express = require("express");
+const handlebars = require("express-handlebars");
+// const bodyParser = require("body-parser"); deprecated
+const app = express();
+// const mongoose = require("mongoose")
+
+//Config
+    //Body Parser
+    // app.use(bodyParser.urlencoded({extended: true}));
+    // app.use(bodyParser.json());
+        app.use(express.urlencoded({extended: true}));
+        app.use(express.json());
+    //Handlebars
+        app.engine("handlebars", handlebars({defaultLayout: "main"}));
+        app.set("view engine", "handlebars");
+
+
+
+//Routes
+
+
+//Others
+const PORT = 8081;
+app.listen(PORT, () => {
+    console.log("Server running...")
+})
