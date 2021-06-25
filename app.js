314c7e968298b4ed78090862cc9df8e580200957
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-06-25 17:15:58 -0300

    MongoDB is working

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const admin = require("./routes/admin");
 const path = require("path");
 
-// const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 
 //Config
 //Body Parser
@@ -17,6 +17,13 @@ app.use(express.json());
 //Handlebars
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+// Mongoose
+mongoose.Promise=global.Promise;
+mongoose.connect("mongodb://localhost/blogapp").then(()=>{
+  console.log("Conectado ao Mongo")
+}).catch((err)=>{
+  console.log(`Erro ao conectar com o Mongo - - ${err}`)
+});
 //public
 app.use(express.static(path.join(__dirname, "public")));
 
