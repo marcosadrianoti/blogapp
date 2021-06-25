90cb0fddef2d6c34b5ead7dab0d653dd1304b33a
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-06-25 14:16:12 -0300

    View add category created

--- app.js
+++ app.js
@@ -9,29 +9,26 @@ const path = require("path");
 // const mongoose = require("mongoose")
 
 //Config
-    //Body Parser
-    // app.use(bodyParser.urlencoded({extended: true}));
-    // app.use(bodyParser.json());
-        app.use(express.urlencoded({extended: true}));
-        app.use(express.json());
-    //Handlebars
-        app.engine("handlebars", handlebars({defaultLayout: "main"}));
-        app.set("view engine", "handlebars");
-    //public
-        app.use(express.static(path.join(__dirname, "public")));
-        
-
-
+//Body Parser
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//Handlebars
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+//public
+app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
-    app.get("/", (req, res)=>{
-        res.send("Página inicial")
-    });
+app.get("/", (req, res) => {
+  res.send("Página inicial");
+});
 
-    app.use("/admin", admin);
+app.use("/admin", admin);
 
 //Others
 const PORT = 8081;
 app.listen(PORT, () => {
-    console.log("Server running...")
-})
+  console.log("Server running...");
+});
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res)=>{
-    res.render("admin/index");
+router.get("/", (req, res) => {
+  res.render("admin/index");
 });
 
-router.get("/posts", (req, res)=>{
-    res.send("Página de posts");
+router.get("/posts", (req, res) => {
+  res.send("Página de posts");
 });
 
-router.get("/categorias", (req, res)=>{
-    res.send("Página de categorias");
+router.get("/categorias", (req, res) => {
+  res.render("admin/categorias");
+});
+
+router.get("/categorias/add", (req, res)=>{
+  res.render("admin/addcategoria");
 });
 
 module.exports = router;
